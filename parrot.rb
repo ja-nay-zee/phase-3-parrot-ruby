c76181bb17b984d80bcac710b56a2c49c0972653
Yaneizy Castillo <yccastillo18@gmail.com>
2021-09-21 01:12:20 -0400

    Completed assignment

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+
+def parrot(phrase = "Squawk!")
+  puts phrase
+  return phrase
+end
+
+# method should accept an argument of a string 
+# both puts out that string
+#  return the string at the end of the method.
+#  should have a default argument of "Squawk!"
